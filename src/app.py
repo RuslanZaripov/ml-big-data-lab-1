d923edc7a4553aa45bc25b39440d63bdf7e82ca7
RuslanZaripov <zaripovruslan864@gmail.com>
2025-02-26 17:09:39 +0300

    increase wait time and log app initialization

--- src/app.py
+++ src/app.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from logger import Logger
+
+SHOW_LOG = True
+logger = Logger(SHOW_LOG)
+log = logger.get_logger(__name__)
 
 app = FastAPI()
+
+log.info('FastAPI app Initialized')
 
 
 @app.get("/")
