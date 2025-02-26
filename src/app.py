1a0c42217dcd77a991476a9929e0e2716288ab1e
RuslanZaripov <zaripovruslan864@gmail.com>
2025-02-26 18:50:58 +0300

    pass json object into end point

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Dict
 from logger import Logger
 
 SHOW_LOG = True
@@ -13,3 +15,16 @@ log.info('FastAPI app Initialized')
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+    
+class PredictionInput(BaseModel):
+    X: List[Dict[str, float]]
+    y: List[Dict[str, float]]
+
+@app.post("/predict")
+async def predict(input_data: PredictionInput):
+    try:
+        X = input_data.X
+        y = input_data.y
+        return {"X": X, "y": y}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
