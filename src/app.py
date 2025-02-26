6d3774968a9f61e820dd0647bfc677b1485e1192
RuslanZaripov <zaripovruslan864@gmail.com>
2025-02-26 19:53:05 +0300

    implement endpoint prediction

--- src/app.py
+++ src/app.py
@@ -2,14 +2,45 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List, Dict
 from logger import Logger
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import traceback
+import sys
+import pickle
+import configparser
+import argparse
 
 SHOW_LOG = True
 logger = Logger(SHOW_LOG)
 log = logger.get_logger(__name__)
 
+config = configparser.ConfigParser()
+config.read("config.ini")
+
 app = FastAPI()
 
-log.info('FastAPI app Initialized')
+log.info('FastAPI app initialized')
+
+parser = argparse.ArgumentParser(description="Web App Model")
+parser.add_argument("-m", "--model",
+                    type=str,
+                    help="Select model",
+                    required=True,
+                    default="LOG_REG",
+                    const="LOG_REG",
+                    nargs="?",
+                    choices=["LOG_REG", "RAND_FOREST", "KNN", "GNB", "D_TREE"])
+args = parser.parse_args()
+
+try:
+    classifier = pickle.load(open(config[args.model]["path"], "rb"))
+except FileNotFoundError:
+    log.error(traceback.format_exc())
+    sys.exit(1)
+
+scaler = StandardScaler()
+
+log.info('web service model initialized')
 
 
 @app.get("/")
@@ -23,8 +54,11 @@ class PredictionInput(BaseModel):
 @app.post("/predict")
 async def predict(input_data: PredictionInput):
     try:
-        X = input_data.X
-        y = input_data.y
-        return {"X": X, "y": y}
+        X = scaler.transform(pd.json_normalize(input_data.X))
+        y = pd.json_normalize(input_data.y)
+        score = classifier.score(X, y)
+        pred = classifier.predict(X)
+        return {"prediction": pred, "score": score}
     except Exception as e:
+        log.error(traceback.format_exc())
         raise HTTPException(status_code=500, detail=str(e))
